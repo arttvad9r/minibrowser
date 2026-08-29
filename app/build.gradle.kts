69f380251023f35de0add0797675a39a2e585991
arttvad9r <arttvad9r@gmail.com>
2026-08-30 01:49:14 +0300

    Stop debug-signing release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
     }
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
