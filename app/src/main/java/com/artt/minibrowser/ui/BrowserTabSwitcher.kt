d7667dd0f243e32007c2b2a2e22967c2e8c4368d
arttvad9r <arttvad9r@gmail.com>
2026-08-30 01:57:22 +0300

    Apply safe drawing insets to tab switcher

--- app/src/main/java/com/artt/minibrowser/ui/BrowserTabSwitcher.kt
+++ app/src/main/java/com/artt/minibrowser/ui/BrowserTabSwitcher.kt
@@ -13,13 +13,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -142,6 +145,7 @@ fun BrowserTabSwitcher(
         Column(
             Modifier
                 .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
                 .graphicsLayer {
                     translationY = (1f - reveal.value) * travelPx
                     alpha = 0.94f + reveal.value * 0.06f
