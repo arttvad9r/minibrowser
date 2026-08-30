32d14e63cdd960c52fab8d1ae862f2eaf3206e25
arttvad9r <arttvad9r@gmail.com>
2026-08-30 20:47:18 +0300

    Make favicon clearing suspendable

--- app/src/main/java/com/artt/minibrowser/browser/BrowserDataClearer.kt
+++ app/src/main/java/com/artt/minibrowser/browser/BrowserDataClearer.kt
@@ -5,7 +5,7 @@ internal class BrowserDataClearer(
     private val clearTabPreviews: () -> Unit,
     private val clearHistory: suspend () -> Unit,
     private val clearBookmarks: suspend () -> Unit,
-    private val clearFaviconCaches: () -> Unit,
+    private val clearFaviconCaches: suspend () -> Unit,
     private val clearWebData: suspend () -> Unit,
 ) {
     suspend fun clear(withBookmarks: Boolean) {
