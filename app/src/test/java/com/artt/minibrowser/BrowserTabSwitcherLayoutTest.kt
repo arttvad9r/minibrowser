157e137eb9a5767ef928b9cbc88e936691e64ba5
arttvad9r <arttvad9r@gmail.com>
2026-08-31 21:29:24 +0300

    Cover adaptive tab grid breakpoints

--- app/src/test/java/com/artt/minibrowser/BrowserTabSwitcherLayoutTest.kt
+++ app/src/test/java/com/artt/minibrowser/BrowserTabSwitcherLayoutTest.kt
@@ -1,5 +1,6 @@
 package com.artt.minibrowser
 
+import androidx.window.core.layout.WindowSizeClass
 import com.artt.minibrowser.ui.tabGridColumnCount
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -7,18 +8,33 @@ import kotlin.test.assertEquals
 class BrowserTabSwitcherLayoutTest {
     @Test
     fun compactWidthUsesTwoColumns() {
-        assertEquals(2, tabGridColumnCount(599f))
+        assertEquals(2, tabGridColumnCount(windowSizeClass(599f)))
     }
 
     @Test
-    fun mediumWidthStartsAt600Dp() {
-        assertEquals(3, tabGridColumnCount(600f))
-        assertEquals(3, tabGridColumnCount(839f))
+    fun mediumWidthUsesThreeColumns() {
+        assertEquals(3, tabGridColumnCount(windowSizeClass(600f)))
+        assertEquals(3, tabGridColumnCount(windowSizeClass(839f)))
     }
 
     @Test
-    fun expandedWidthStartsAt840Dp() {
-        assertEquals(4, tabGridColumnCount(840f))
-        assertEquals(4, tabGridColumnCount(1200f))
+    fun expandedWidthUsesFourColumns() {
+        assertEquals(4, tabGridColumnCount(windowSizeClass(840f)))
+        assertEquals(4, tabGridColumnCount(windowSizeClass(1199f)))
     }
+
+    @Test
+    fun largeWidthUsesFiveColumns() {
+        assertEquals(5, tabGridColumnCount(windowSizeClass(1200f)))
+        assertEquals(5, tabGridColumnCount(windowSizeClass(1599f)))
+    }
+
+    @Test
+    fun extraLargeWidthUsesSixColumns() {
+        assertEquals(6, tabGridColumnCount(windowSizeClass(1600f)))
+        assertEquals(6, tabGridColumnCount(windowSizeClass(2000f)))
+    }
+
+    private fun windowSizeClass(widthDp: Float): WindowSizeClass =
+        WindowSizeClass(widthDp, 800f)
 }
