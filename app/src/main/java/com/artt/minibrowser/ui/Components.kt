bc6d697aaf1522874b0c8e3764ba4961239c2f42
arttvad9r <arttvad9r@gmail.com>
2026-08-31 23:16:49 +0300

    Use compatible fully expanded sheet state

--- app/src/main/java/com/artt/minibrowser/ui/Components.kt
+++ app/src/main/java/com/artt/minibrowser/ui/Components.kt
@@ -36,12 +36,11 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberBottomSheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -369,10 +368,7 @@ fun BrowserBottomSheet(
     onDismissRequest: () -> Unit,
     content: @Composable (dismissThen: (after: () -> Unit) -> Unit) -> Unit,
 ) {
-    val state = rememberBottomSheetState(
-        initialValue = SheetValue.Hidden,
-        enabledValues = setOf(SheetValue.Hidden, SheetValue.Expanded),
-    )
+    val state = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val scope = rememberCoroutineScope()
     var dismissing by remember { mutableStateOf(false) }
 
