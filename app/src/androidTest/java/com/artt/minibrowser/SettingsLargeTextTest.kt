c91ede4fbf819c6795c62148c308ce77c034f94f
arttvad9r <arttvad9r@gmail.com>
2026-09-04 10:40:47 +0300

    fix: use compose rule node lookup in settings test

--- app/src/androidTest/java/com/artt/minibrowser/SettingsLargeTextTest.kt
+++ app/src/androidTest/java/com/artt/minibrowser/SettingsLargeTextTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.v2.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performScrollToNode
 import androidx.compose.ui.unit.Density
