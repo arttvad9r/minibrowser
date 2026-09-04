9c79eab82fe10adacb00991b819b82be86642f96
arttvad9r <arttvad9r@gmail.com>
2026-09-04 10:40:34 +0300

    fix: use compose rule node lookup in accessibility test

--- app/src/androidTest/java/com/artt/minibrowser/AccessibilityChecksTest.kt
+++ app/src/androidTest/java/com/artt/minibrowser/AccessibilityChecksTest.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.junit4.accessibility.enableAccessibilityChecks
 import androidx.compose.ui.test.junit4.v2.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.tryPerformAccessibilityChecks
 import androidx.test.ext.junit.runners.AndroidJUnit4
