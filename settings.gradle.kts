6ac255ae07bfcd1365f378f5b6d306ad55625bf6
arttvad9r <arttvad9r@gmail.com>
2026-09-07 23:12:32 +0300

    build: isolate GeckoView repository content

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,26 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral(); maven("https://maven.mozilla.org/maven2") } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        exclusiveContent {
+            forRepository {
+                maven("https://maven.mozilla.org/maven2")
+            }
+            filter {
+                includeGroup("org.mozilla.geckoview")
+            }
+        }
+    }
+}
+
 rootProject.name = "minibrowser"
 include(":app", ":benchmark")
