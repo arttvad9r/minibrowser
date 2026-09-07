2915e478a15ecb954226496dc33db445a5926f80
arttvad9r <arttvad9r@gmail.com>
2026-09-07 22:08:28 +0300

    fix: remove unused Gecko result expression

--- app/src/main/java/com/artt/minibrowser/browser/SiteSettingsController.kt
+++ app/src/main/java/com/artt/minibrowser/browser/SiteSettingsController.kt
@@ -70,9 +70,8 @@ internal class SiteSettingsController(
     }
 
     suspend fun clearSiteData(host: String): Result<Unit> = runCatching {
-        storageController.clearDataFromHost(host, StorageController.ClearFlags.SITE_DATA)
-            .awaitValue()
-            .let { Unit }
+        storageController.clearDataFromHost(host, StorageController.ClearFlags.SITE_DATA).awaitValue()
+        Unit
     }
 }
 
