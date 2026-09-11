02183fa50c45422edf653f344c86a6f1fd19921c
artt <arttvad9r@gmail.com>
2026-09-11 17:38:42 +0300

    test: cover Gecko default callback forwarding

--- app/src/test/java/com/artt/minibrowser/engine/AndroidComponentsUiCompatibilityDelegatesTest.kt
+++ app/src/test/java/com/artt/minibrowser/engine/AndroidComponentsUiCompatibilityDelegatesTest.kt
@@ -45,4 +45,48 @@ class AndroidComponentsUiCompatibilityDelegatesTest {
         assertEquals(1, calls.get())
         assertSame(expectedResult, result)
     }
+
+    @Test
+    fun navigationDelegateForwardsUnobservedJavaDefaultCallbacks() {
+        val session = GeckoSession()
+        var forwarded: Boolean? = null
+        val delegate =
+            object : GeckoSession.NavigationDelegate {
+                override fun onCanGoBack(session: GeckoSession, canGoBack: Boolean) {
+                    forwarded = canGoBack
+                }
+            }
+        val wrapper =
+            AndroidComponentsNavigationUiCompatibilityDelegate(
+                delegate = delegate,
+                sessionId = "tab",
+                compatibilityState = AndroidComponentsUiCompatibilityState(),
+            )
+
+        wrapper.onCanGoBack(session, true)
+
+        assertEquals(true, forwarded)
+    }
+
+    @Test
+    fun progressDelegateForwardsUnobservedJavaDefaultCallbacks() {
+        val session = GeckoSession()
+        var forwarded: Int? = null
+        val delegate =
+            object : GeckoSession.ProgressDelegate {
+                override fun onProgressChange(session: GeckoSession, progress: Int) {
+                    forwarded = progress
+                }
+            }
+        val wrapper =
+            AndroidComponentsProgressUiCompatibilityDelegate(
+                delegate = delegate,
+                sessionId = "tab",
+                compatibilityState = AndroidComponentsUiCompatibilityState(),
+            )
+
+        wrapper.onProgressChange(session, 42)
+
+        assertEquals(42, forwarded)
+    }
 }
